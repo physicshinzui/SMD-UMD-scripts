02778fc2d91b24e16ede27ffe812a7dfc78c0ccc
Shinji Iida <natsure@siida-macbook.local>
2021-07-05 16:38:05 +0900

    trivial

--- 01_eq/02_eq.sh
+++ 01_eq/02_eq.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -Ceu
 cat << EOS
-Author: Shinji Iida 3.6.2019 
+Written by Shinji Iida 
+Date: 5.7.2021
 This script submits MD runs.
     Usage:
         bash ${0} [run id] 
 EOS
 id=$1
 
-bash check_mdpfiles.sh
-
 read -p "Proceed? Enter"
 
 echo "Making md inputs...(nvt.mdp and npt.mdp)"
-cat templates/template_nvt.mdp | sed -e "s!#{RAND}!${RANDOM}!g" > nvt_eq_${id}.mdp
-cp  templates/template_npt.mdp npt_eq_${id}.mdp
+cat templates/nvt_eq.mdp | sed -e "s!#{RAND}!${RANDOM}!g" > nvt_eq_${id}.mdp
+cp  templates/npt_eq.mdp npt_eq_${id}.mdp
 
 echo "NVT equilibration runs are running..."
 gmx grompp -f nvt_eq_${id}.mdp \
